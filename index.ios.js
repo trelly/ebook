533c907b81a059d2052a4f9e423e183208898c2e
gaotianyi <gaotianyi@bytedance.com>
2016-10-09 15:53:41 +0800

    初始化项目
    Change-Id: I53a0aef11882dcd020763a5852bda7d6ce9c2865

---
+++ index.ios.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class ebook extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          ebook
+        </Text>
+        <Text style={styles.instructions}>
+          小数阅读器，侧边栏
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+    paddingTop: 10,
+    position: 'relative',
+    top: 10
+  },
+});
+
+AppRegistry.registerComponent('ebook', () => ebook);
